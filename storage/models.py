8be111a10877076f7427eadcd5ed4834afdcf0ec
Jhones <jhonesaly@gmail.com>
2023-04-13 19:12:47 -0300

    feat: create model

--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    author = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=255)
+    edition = models.IntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.IntegerField()
