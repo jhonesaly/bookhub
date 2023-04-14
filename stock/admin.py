7d5a6cd4f289cb1d4d2c8eaf309688f8a7ce9c03
Jhones <jhonesaly@gmail.com>
2023-04-14 13:44:02 -0300

    feat: admin content

--- stock/admin.py
+++ stock/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'publisher', 'price', 'quantity')
+    search_fields = ('id', 'title', 'author', 'publisher')
+    list_filter = ('publisher', 'author')
+    ordering = ('title',)
+
+admin.site.register(Book, BookAdmin)
