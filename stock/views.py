949d990a55d8af643aa16392b363ed1685c2a114
Jhones <jhonesaly@gmail.com>
2023-04-16 08:41:12 -0300

    feat: view of form

--- stock/views.py
+++ stock/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from stock.forms import BookForm
 
 def index(request):
     return HttpResponse("Hello, world. You're at index.")
+
+def form(request):
+    return render(request, "form.html")
