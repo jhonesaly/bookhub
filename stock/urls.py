91ca9c5db1b4ee240857a16948a0ed7a1bea9780
Jhones <jhonesaly@gmail.com>
2023-04-17 10:19:42 -0300

    fix: remove forms (will use admin area)

--- stock/urls.py
+++ stock/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("form/", views.form, name="form"),
 ]
--- stock/views.py
+++ stock/views.py
@@ -1,25 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from stock.forms import BookForm
 
 def index(request):
     return HttpResponse("Hello, world. You're at index.")
-
-def form(request):
-    if request.method == "GET":
-        form = BookForm()
-        context = {
-            'form': form
-        }
-        return render(request, "form.html", context=context)
-    else: 
-        form = BookForm(request.POST)
-        if form.is_valid():
-            book = form.save()
-            form = BookForm()
-            context = {
-                'form': form
-            }
-            return render(request, "form.html", context=context)
-
-        
