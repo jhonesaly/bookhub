18f83c54f6d4106603bbc8685cfc0065b51b31ab
Jhones <jhonesaly@gmail.com>
2023-04-19 08:32:52 -0300

    feat: new urls

--- config/urls.py
+++ config/urls.py
@@ -22,8 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html")),
-    path('stock/', include("stock.urls")),
+    path('', include("stock.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
