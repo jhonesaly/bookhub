5bfa818e5132320121670eb64a8ff1cd2abfdbd2
Jhones <jhonesaly@gmail.com>
2023-04-19 15:38:49 -0300

    feat: change name of url

--- stock/urls.py
+++ stock/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.BookListView.as_view(), name="list"),
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name="detail")
+    path('', views.BookListView.as_view(), name="book_list"),
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name="book_detail")
 ]
