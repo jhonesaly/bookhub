515b6eb53e053f416c5a9ceea8f2bb228d3ab6ec
Jhones <jhonesaly@gmail.com>
2023-04-19 09:12:18 -0300

    feat: move files to app template folder

--- stock/views.py
+++ stock/views.py
@@ -4,23 +4,23 @@ from .models import Book, Author, Category
 
 class BookListView(ListView):
     model = Book
-    template_name = 'book_list.html'
+    template_name = 'stock/book_list.html'
     context_object_name = 'book_list'
 
 
 class BookDetailView(DetailView):
     model = Book
-    template_name = 'book_detail.html'
+    template_name = 'stock/book_detail.html'
     context_object_name = 'book'
 
 
 class AuthorListView(ListView):
     model = Author
-    template_name = 'author_list.html'
+    template_name = 'stock/author_list.html'
     context_object_name = 'author_list'
 
 
 class CategoryListView(ListView):
     model = Category
-    template_name = 'category_list.html'
+    template_name = 'stock/category_list.html'
     context_object_name = 'category_list'
