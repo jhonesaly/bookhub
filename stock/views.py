5d3e6da5daddb847410fb60552444adae58ca49b
Jhones <jhonesaly@gmail.com>
2023-04-19 09:07:34 -0300

    feat: category and author list view

--- stock/views.py
+++ stock/views.py
@@ -1,14 +1,26 @@
 from django.views.generic import ListView, DetailView
-from .models import Book
+from .models import Book, Author, Category
 
 
 class BookListView(ListView):
     model = Book
-    template_name = 'list.html'
+    template_name = 'book_list.html'
     context_object_name = 'book_list'
+
 
 class BookDetailView(DetailView):
     model = Book
-    template_name = 'detail.html'
+    template_name = 'book_detail.html'
     context_object_name = 'book'
 
+
+class AuthorListView(ListView):
+    model = Author
+    template_name = 'author_list.html'
+    context_object_name = 'author_list'
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category_list.html'
+    context_object_name = 'category_list'
