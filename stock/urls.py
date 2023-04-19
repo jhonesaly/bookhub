308296cc1397977354c6ce421d4d55e674c39b15
Jhones <jhonesaly@gmail.com>
2023-04-19 08:36:37 -0300

    feat: new app urls

--- stock/urls.py
+++ stock/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.BookListView.as_view(), name="list"),
+    path('', views.BookListView.as_view(), name="list"),
     path('books/<int:pk>/', views.BookDetailView.as_view(), name="detail")
 ]
